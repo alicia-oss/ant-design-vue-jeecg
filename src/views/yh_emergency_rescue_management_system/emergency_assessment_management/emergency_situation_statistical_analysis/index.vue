cfe68888ae362d4b148472c064c8b0a90f5d937c
55488 <554884007@qq.com>
2021-09-19 11:39:12 +0800

    9.19

--- src/views/yh_emergency_rescue_management_system/emergency_assessment_management/emergency_situation_statistical_analysis/index.vue
+++ src/views/yh_emergency_rescue_management_system/emergency_assessment_management/emergency_situation_statistical_analysis/index.vue
@@ -1,22 +1,29 @@
 <template>
   <a-card :bordered="false">
-    <div class="table">
-      <div class="item">
-        <bar title="近四年每年发生事故数量"></bar>
-      </div>
-      <div class="item">
-        <div>
-          <a-select placeholder="请选择年份" style="width: 50%;">
-            <a-select-option v-for="item in inputData.year" :value="item">
-              {{item}}
-            </a-select-option>
-          </a-select>
+    <a-tabs default-active-key="1" @change="callback">
+      <a-tab-pane key="1" tab="事故统计">
+        Content of Tab Pane 1
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="分析报表" force-render>
+        <div class="table">
+          <div class="item">
+            <bar title="近四年每年发生事故数量"></bar>
+          </div>
+          <div class="item">
+            <div>
+              <a-select placeholder="请选择年份" style="width: 50%;">
+                <a-select-option v-for="item in inputData.year" :value="item">
+                  {{item}}
+                </a-select-option>
+              </a-select>
+            </div>
+            <div>
+              <pie title="各级别事故占比"></pie>
+            </div>
+          </div>
         </div>
-        <div>
-          <pie title="各级别事故占比"></pie>
-        </div>
-      </div>
-    </div>
+      </a-tab-pane>
+    </a-tabs>
   </a-card>
 </template>
 
